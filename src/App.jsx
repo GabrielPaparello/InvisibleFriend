a43eda6b8a0ac604708a1e0ce8ee4309757fc597
unknown <gabriel.alejandro.paparello@gmail.com>
2024-05-18 10:56:47 -0300

    asdas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Nav } from './utils/Nav'
+import { Nav } from './utils/Nav'; 
+import React from 'react';
+// import imagen from './assets/invisibleApp.png';
 
 export const App = () => {
   return (
-      <>
+    <>
       <Nav />
-      
-      
-      
-      </>
-    
-
-)
-}
+      <div>
+        <h2>Invisible Friends</h2>
+        {/* {imagen && <img src={imagen} alt="" />} */}
+      </div>
+    </>
+  );
+};
