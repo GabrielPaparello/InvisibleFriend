b2b788bd896d64b64112d2c87a2594f394908cc3
unknown <gabriel.alejandro.paparello@gmail.com>
2024-05-18 10:57:09 -0300

    asdas

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
-// import { InvisibleFriend } from './pages/invisibleApp/InvisibleFriend'
-import { App } from './App'
+import { InvisibleFriend } from './pages/invisibleApp/InvisibleFriend'
+// import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    {/* <InvisibleFriend /> */}
+    {/* <App /> */}
+    <InvisibleFriend />
   </React.StrictMode>,
 )
