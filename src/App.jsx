b1bc8a0dee1a17d84a03780693cdc8db205802a7
unknown <gabriel.alejandro.paparello@gmail.com>
2024-05-18 10:15:06 -0300

    Creating /

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Nav } from './utils/Nav'
+
+export const App = () => {
+  return (
+      <>
+      <Nav />
+      
+      
+      
+      </>
+
+
+)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
 import { InvisibleFriend } from './pages/InvisibleFriend/InvisibleFriend'
-
+import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <App></App> */}
     <InvisibleFriend />
   </React.StrictMode>,
 )
