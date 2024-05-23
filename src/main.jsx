2ca76915fdd999cba802df31a6221cf9b073c713
unknown <gabriel.alejandro.paparello@gmail.com>
2024-05-23 12:56:22 -0300

    404

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='*' element={<Home />}/>
         <Route path='/InvisibleFriend' element={<InvisibleFriend />} />
         </Routes>
     </BrowserRouter>
